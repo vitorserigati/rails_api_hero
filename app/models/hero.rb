03df6095039e3048672eda5a1f18a3d863e95ad1
Vitor Serigati de Oliveira Basso <79308595+vitorserigati@users.noreply.github.com>
2023-01-18 20:52:59 -0300

    changed crud to verify token on db

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,5 +1,7 @@
 class Hero < ApplicationRecord
   validates :name, :token, presence: true
+
+  scope :by_token, ->(value) { where(token: value) }
   scope :sorted_by_name, -> { order(:name) }
   scope :search, ->(term) { where('LOWER(name) LIKE?', "%#{term.downcase}%") if term.present? }
 end

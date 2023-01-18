326fd129692648c7b0e2e3a85d707cdc54d9f15d
Vitor Serigati de Oliveira Basso <79308595+vitorserigati@users.noreply.github.com>
2023-01-18 20:22:49 -0300

    added token column in heroes

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,5 +1,5 @@
 class Hero < ApplicationRecord
-  validates :name, presence: true
+  validates :name, :token, presence: true
   scope :sorted_by_name, -> { order(:name) }
   scope :search, ->(term) { where('LOWER(name) LIKE?', "%#{term.downcase}%") if term.present? }
 end
